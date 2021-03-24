339c802edd27ead2c95d1a6135c882192a96ae96
Beth <bethm.meeker@gmail.com>
2021-03-24 14:56:28 -0600

    Write functions for book.js, remove skips

--- src/book.js
+++ src/book.js
@@ -1,12 +1,51 @@
-function createTitle() {
-  
+function createTitle(title) {
+  return `The ${title}`;
 }
+
+function buildMainCharacter(name, age, pronouns) {
+  var newCharacter = {
+    name: name,
+    age: age,
+    pronouns: pronouns,
+  }
+  return newCharacter;
+}
+
+function saveReview(newReview, reviews) {
+  if (reviews.includes(newReview)) {
+    return reviews;
+  } else {
+    reviews.push(newReview);
+    return reviews;
+  }
+}
+
+function calculatePageCount(bookTitle) {
+  var pageCount = bookTitle.length * 20;
+  return pageCount;
+}
+
+function writeBook(title, character, genre) {
+  var book = {
+    title: title,
+    mainCharacter: character,
+    pageCount: calculatePageCount(title),
+    genre: genre,
+  }
+  console.log(book);
+  return book;
+}
+
+function editBook(book) {
+  book.pageCount = (book.pageCount / 4) * 3;
+}
+
 
 module.exports = {
   createTitle,
-  // buildMainCharacter,
-  // saveReview,
-  // calculatePageCount,
-  // writeBook,
-  // editBook
+  buildMainCharacter,
+  saveReview,
+  calculatePageCount,
+  writeBook,
+  editBook
 }
